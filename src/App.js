0bc708f0546bd90c56389200a62eabd31f36a6fe
Ishan <ishannagar@Ishans-MacBook-Air.local>
2024-10-08 21:35:16 +0530

    add block component and design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from './Components/Navbar';
 import './App.css';
+import Blocks from './components/blocks';
 
 function App() {
   return (
+<<<<<<< HEAD
     <Navbar />
+=======
+    <div className=''>
+     <Blocks />
+    </div>
+>>>>>>> 850f73b (add block component and design)
   );
 }
 
 export default App
-// changes
+
 

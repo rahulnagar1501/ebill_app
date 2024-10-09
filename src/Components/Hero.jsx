5a5011b547a91e86882babc6458e0a12a4c26630
Rahul Nagar <rahulnagar1501@gmail.com>
2024-10-10 01:00:08 +0530

    changed the folder structure

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import image from '../assets/image.png'
+import image from './Assets/image.png'
 
 const Hero = () => {
     return (
 
         <div className='md:py-16' style={{ backgroundImage: `url(${image})`, boxShadow: 'inset 0px 1px 4px rgba(0, 0, 0, 0.3)', }}>
             <div className='px-10 py-4 md:px-[8%] flex flex-col items-center gap-10 max-w-[840px] mx-auto'>
-                <p className='border table py-1 px-2 rounded-full text-purple-600 border-purple-600 bg-purple-100'>Modern design</p>
+                <p className='border table py-1 px-2 rounded-full text-purple-600 border-purple-600 bg-purple-100 '>Modern design</p>
                 <h3 className='text-5xl font-bold text-center'>Kickstart your project with Block</h3>
                 <h4 className='text-gray-500 font-semibold text-xl text-center'>
                     Block built with a modular web design concept will easily kickstart your project, and build website & marketing sites faster.
